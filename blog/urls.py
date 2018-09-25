a8a757151bd94b666846db40cdf7672cfa113902
babonaresh <nbashaboena@unomaha.edu>
2018-09-25 18:58:39 -0500

    updated with sahare and comment code

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
    # path('post/new/', views.post_new, name='post_new'),
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    #path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('tag/<slug:tag_slug>/',
          views.post_list, name='post_list_by_tag'),
