0f4cc81a3c0a7bc3869e342ebdd1634a89703477
babonaresh <nbashaboena@unomaha.edu>
2018-09-25 10:24:57 -0500

    updated with sahare and comment code

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,37 @@
-from django.shortcuts import redirect
-from .forms import PostForm
-from django.shortcuts import render, get_object_or_404
-from django.utils import timezone
-from .models import Post
-from .forms import EmailPostForm
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from django.core.mail import send_mail
+from django.db.models import Count
+from django.utils import timezone
+from taggit.models import Tag
 from .models import Post, Comment
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, PostForm
 
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
 
+    paginator = Paginator(object_list, 3)  # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request,
+                  'blog/post/list.html',
+                  {'page': page,
+                   'posts': posts,
+                   'tag': tag})
 def post_new(request):
      if request.method == "POST":
         form = PostForm(request.POST)
